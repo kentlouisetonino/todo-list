881ad61e590007deb875a8a4113c2db63989aea4
Kent Louise G. Tonino <69438999+kentlouisetonino@users.noreply.github.com>
2022-10-03 21:03:20 +0800

    Refactor and Add Prettier (#1)

---
+++ src/components/List/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import Item from '../Item'
+
+const List = () => {
+  const todos = useSelector((state) => state.todos)
+
+  return (
+    <ul className='list-group'>
+      {todos.map((todo, index) => (
+        <Item
+          key={index}
+          id={todo.id}
+          title={todo.title}
+          completed={todo.completed}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default List
