7d6db895c2bee68a5e77af957dfc8b7b5c339ce8
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2022-10-31 00:33:50 +0800

    Remove Responsive Design

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import App from './App'
 import './index.css'
 import store from './redux/store'
+import App from './app'
 
 ReactDOM.render(
   <React.StrictMode>
